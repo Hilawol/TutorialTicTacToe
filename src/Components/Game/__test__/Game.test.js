3c7b55895f161bd0fca6ca77eba1af1c0396f706
Hila <hila.kershenovich@solarwinds.com>
2022-01-10 11:57:58 +0200

    added test with testId

--- src/Components/Game/__test__/Game.test.js
+++ src/Components/Game/__test__/Game.test.js
@@ -6,49 +6,68 @@ import {log} from "util";
 describe("Square:",()=>{
 
     it("should render all squares initially without content",()=>{
-        render(<Game/>);
+       act(()=>{
+            render(<Game/>);
+        })
         const buttonElements = screen.getAllByRole("button");
         const squares = buttonElements.filter(button => button.classList.contains("square"));
         squares.forEach(square => expect(square.textContent).toBe(""));
     });
 
     it("should render next players sign after clicking it, when it's clicked for the first time", ()=>{
-        render(<Game/>);
+        act(()=>{
+            render(<Game/>);
+        })
         const buttonElements = screen.getAllByRole("button");
         const squares = buttonElements.filter(button => button.classList.contains("square"));
         const divElement = screen.getByText(/next player/i);
         const nextPlayer = divElement.textContent.slice(-1);
-        fireEvent.click(squares[0]);
+        act(()=>{
+            fireEvent.click(squares[0]);
+        })
         expect(squares[0].textContent).toBe(nextPlayer);
     })
 
     it("should not change it's content after clicking it, if square was already clicked", ()=>{
-        render(<Game/>);
+        act(()=>{
+            render(<Game/>);
+        })
         const buttonElements = screen.getAllByRole("button");
         const squares = buttonElements.filter(button => {
             return button.classList.contains("square");
         });
-        fireEvent.click(squares[0]);
+        act(()=>{
+            fireEvent.click(squares[0]);
+        })
         const squareText = squares[0].textContent;
-        fireEvent.click(squares[0]);
+        act(()=>{
+            fireEvent.click(squares[0]);
+        })
         expect(squares[0].textContent).toBe(squareText);
     })
 });
 
 describe ("Game info",()=>{
     it(('should render "Go to game start" button when loading'),()=>{
-        render(<Game/>);
+        act(()=>{
+            render(<Game/>);
+        })
         const buttonElements = screen.getByText(/go to game start/i);
         expect(buttonElements).toBeInTheDocument();
     });
 
     it('should rerender the board to the initial state after clicking the "Go to game start"',()=>{
-        render(<Game/>);
-        const buttonElements = screen.getAllByRole("button");
-        const squares = buttonElements.filter(button => button.classList.contains("square"));
-        fireEvent.click(squares[0]);
-        const goToStartButton = buttonElements.find(button => button.textContent === "Go to game start");
-        fireEvent.click(goToStartButton);
+        act(()=>{
+            render(<Game/>);
+        })
+        const squares = screen.getAllByTestId("square");
+        act(()=>{
+            fireEvent.click(squares[0]);
+        })
+        const goToStartButton = screen.getByRole("button", {name:/go to game start/i});
+        act(()=>{
+            fireEvent.click(goToStartButton);
+        })
         squares.forEach(square => expect(square.textContent).toBe(""));
     });
 })
